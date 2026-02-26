1c269ff1252b2d7e46a041cca41dc05977336401
Julius Cezar Tapar <j_tapar@cda.gov.ph>
2026-02-26 18:53:26 +0800

    Update card grid

--- netflix-clone/app/my-list/page.tsx
+++ netflix-clone/app/my-list/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Container from '@/components/Container';
 import MovieCard from '@/components/moviecard/MovieCard';
 import useMyList from '@/lib/useMyList';
 import { Movie } from '@/types/movie';
@@ -7,8 +8,17 @@ import Link from 'next/link';
 function MyListPage() {
   const { data } = useMyList();
 
+  if (data.length === 0) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold">My List</h1>
+        <p className="text-gray-500">No movies in your list</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="grid grid-cols-5 gap-4 px-10 my-10">
+    <Container>
       {data.map((item: Movie) => (
         <Link
           key={item.id}
@@ -17,7 +27,7 @@ function MyListPage() {
           <MovieCard title={item.title || ''} poster_path={item.poster_path} />
         </Link>
       ))}
-    </div>
+    </Container>
   );
 }
 
--- netflix-clone/components/Container.tsx
+++ netflix-clone/components/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-1 gap-4 px-10">
+    <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 px-10">
       {children}
     </div>
   );
